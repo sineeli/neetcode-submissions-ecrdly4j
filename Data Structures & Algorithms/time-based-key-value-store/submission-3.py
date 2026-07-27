b3592853a1993b18f3033a1560b9724242ec2501
Siva Sravana Kumar Neeli <113718461+sineeli@users.noreply.github.com>
2026-07-27 15:39:54 -0500

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,23 @@
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = {}
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.time_map:
+            self.time_map[key] = []
+        self.time_map[key].append((value, timestamp))
+        
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = "", self.time_map.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            mid = (r - l) // 2 + l
+            if timestamp >= values[mid][1]:
+                res = values[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return res
+        
