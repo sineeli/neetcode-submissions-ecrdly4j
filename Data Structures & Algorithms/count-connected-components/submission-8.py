f97366c709e2e22b43844920a5f13020408ea540
Siva Sravana Kumar Neeli <113718461+sineeli@users.noreply.github.com>
2026-08-29 19:47:35 -0500

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.py
@@ -0,0 +1,45 @@
+class DSU:
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+        for i in range(n):
+            self.par[i] = i
+            self.rank[i] = 0
+    
+    def find(self, p):
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        
+        return p
+    
+    def union(self, u, v):
+        p1, p2 = self.find(u), self.find(v)
+        
+        if p1 == p2:
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p1] += 1
+        
+        return True
+    
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = n 
+        for a, b in edges:
+            if dsu.union(a, b):
+                res -= 1
+        
+        return res
+
+
+        
